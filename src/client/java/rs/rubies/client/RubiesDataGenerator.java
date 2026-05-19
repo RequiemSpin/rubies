f4102b01d243f4a45d589d07d44c6c127fc070be
RequiemSpin <requiemspi@gmail.com>
2026-05-19 20:21:19 +0100

    v0.2 P2

--- src/client/java/rs/rubies/client/RubiesDataGenerator.java
+++ src/client/java/rs/rubies/client/RubiesDataGenerator.java
@@ -2,10 +2,13 @@ package rs.rubies.client;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import rs.rubies.client.datagen.ModModelProvider;
 
 public class RubiesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
---
+++ src/main/java/rs/rubies/item/ModItems.java
@@ -0,0 +1,32 @@
+package rs.rubies.item;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import rs.rubies.Rubies;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item RUBY = registerItem("ruby", Item::new);
+    //public static final Item RUBY = registerItem("ruby", properties -> new Item(properties);
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(Rubies.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Rubies.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        Rubies.LOGGER.info("Registering Items - " + Rubies.MOD_ID);
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS).register(output -> {
+            output.accept(RUBY);
+        });
+    }
+}
